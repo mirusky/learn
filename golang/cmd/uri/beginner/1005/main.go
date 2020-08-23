6bb32d5672d32c66ba9ec418c2e96f6eff6ef8b6
João Victor Santos <profissional.joaovss@gmail.com>
2020-08-22 22:39:45 -0300

    feat(uri-beginner): solution question 1005

---
+++ golang/cmd/uri/beginner/1005/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var A, B, MEDIA float64
+	fmt.Scanf("%f", &A)
+	fmt.Scanf("%f", &B)
+	MEDIA = ((A * 3.5) + (B * 7.5)) / 11
+	fmt.Printf("MEDIA = %.5f\n", MEDIA)
+}
